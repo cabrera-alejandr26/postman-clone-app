01ce8413c54b4effc3defa4e8b2d2aec2ddfdf4b
cabrera-alejandr26 <80721797+cabrera-alejandr26@users.noreply.github.com>
2024-11-19 14:14:30 -0600

    Initial project changes: Changed UI and Library Project Settings.

--- PostmanCloneUI/Dashboards.cs
+++ PostmanCloneUI/Dashboards.cs
@@ -1,8 +1,8 @@
 namespace PostmanCloneUI
 {
-    public partial class Form1 : Form
+    public partial class Dashboards : Form
     {
-        public Form1()
+        public Dashboards()
         {
             InitializeComponent();
         }
